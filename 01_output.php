6ef9d748c69a54ca5f1e3e1b82d90004872ef68b
Jeff <jbridges1119@gmail.com>
2022-10-31 17:46:38 -0600

    Basic outputs explanation

--- 01_output.php
+++ 01_output.php
@@ -1,3 +1,31 @@
+<!-- Like EJS alligator tags -->
 <?php
- echo "hel3lo222112aazzzz232aa22";
+ echo "test";
+ //inside comments
  ?>
+
+<!--
+ comment outside
+  -->
+
+
+ <?php
+ // echo - Output Multiple strings, numbers, html, etc
+ echo 123, 'Hello', 10.5;
+// 123Hello10.5
+
+ //print - Works like echo, but can only take in a single argument
+ print 123;
+//123
+
+ // print_() - Print single values and arrays
+ print_r([1,2,3]);
+//Array ( [0] => 1 [1] => 2 [2] => 3 )
+
+ //var_dump() - Returns more info like data type and length
+ var_dump('Hello');
+//string(5) "Hello"
+
+ // var_export() - Similar to var_dump(). Outputs a string representation of a variable
+ var_export('Hello');
+ //'Hello'
